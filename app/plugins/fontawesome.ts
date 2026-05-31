0993db9537bca978b9569e5e0c24eca95f919bdc
AnaMelisaGo <melisago.rockn@gmail.com>
2026-05-31 21:18:25 +0200

    Fix issues in navbar

---
+++ app/plugins/fontawesome.ts
@@ -0,0 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas)
+library.add(fab)
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon)
+})
