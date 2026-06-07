de9315cdd579cd6fd060347c550c215515c98158
AnaMelisaGo <melisago.rockn@gmail.com>
2026-06-07 15:51:51 +0200

    Add reusable hero component

---
+++ app/components/ReusableHero.vue
@@ -0,0 +1,85 @@
+<script setup>
+defineProps({
+    heroTitle: {
+        type: String,
+        required: true,
+    },
+    heroCaption: {
+        type: String,
+        required: true,
+    },
+    buttonText: {
+        type: String,
+        default: null,
+    },
+    buttonLink: {
+        type: String,
+        default: null,
+    },
+    backgroundImage: {
+        type: String,
+        // default: '#fff',
+    }
+})
+</script>
+<template>
+    <div class="reusable-hero" :style="{backgroundImage: backgroundImage ? `linear-gradient(rgb(0 0 0 / 5%) 46%, rgba(0, 0, 0, 0.6) 100%), url(${backgroundImage})`: 'none'}">
+        <div class="hero-container">
+            <h2 class="title">
+                {{ heroTitle }}
+    
+            </h2>
+    
+            <p class="caption">
+                {{ heroCaption }}
+    
+            </p>
+            <NuxtLink v-if="buttonText && buttonLink" :to="buttonLink" class="hero-btn">{{ buttonText }}</NuxtLink>
+        </div>
+    </div>
+</template>
+<style scoped>
+.reusable-hero {
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 85vh;
+    padding: 6rem 2rem;
+}
+
+.reusable-hero .hero-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    height: 100%;
+    color: white;
+}
+
+.hero-container .title {
+    font-size: 4rem;
+    opacity: 0;
+
+    animation: slideDown 1s ease-out forwards;
+    transition: transform 1s ease-in-out;
+}
+
+.hero-container .caption {
+    opacity: 0;
+    animation: slideDown 1s ease-in-out forwards;
+    transition: transform 1s ease-in-out;
+    animation-delay: .5s;
+}
+
+@keyframes slideDown {
+    from {
+        opacity: 0;
+        transform: translateY(-40px);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+</style>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,4 +1,11 @@
 <template>
+    <section>
+        <ReusableHero
+        hero-title = "Title"
+        hero-caption = "caption"
+        background-image="/images/fundacion-astra-zeneca.png"
+        />
+    </section>
     <div>
         <h1 class="text-3xl font-bold font-heading">About Us</h1>
         <p>This is the about page for Ikigai Advertising.</p>
